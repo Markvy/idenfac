f029a50fd96c8c44959c94d234e4019515e076aa
Markvy <markyag@gmail.com>
2017-04-28 16:18:37 -0400

    Update hosts.py
    inline function for no good reason

--- src/hosts.py
+++ src/hosts.py
@@ -1,15 +1,13 @@
 from time import time
 from collections import Counter
 from heapq import nsmallest
-def top10(counter): # Counter.most_common doesn't do lexicographic order
-  return nsmallest(10, zip(counter.values(), counter))
 hosts = Counter()
 begin = time()
 for line_num, line in enumerate(open('../log_input/log.txt'),1):
   host = line.split(' ', 1)[0]
   hosts[host] -= 1 # -= instead of += is hack for lexicographic order
 top_hosts = open('../log_output/hosts.txt', 'w')
-for count, host in top10(hosts):
+for count, host in nsmallest(10, zip(hosts.values(), hosts)):
   print >>top_hosts, host+','+`-count`
 del top_hosts
 if line_num > 10**6: print 'hosts took %g seconds'%(time() - begin)
